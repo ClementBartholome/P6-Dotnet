ef35a402c2fab17aa44dacea8ae1a8c13b7ff509
Clément Bartholomé <123072377+ClementBartholome@users.noreply.github.com>
2024-12-07 17:55:49 +0100

    Add files via upload

--- P6-Dotnet.sql
+++ P6-Dotnet.sql
@@ -2,41 +2,52 @@ CREATE DATABASE P6_Dotnet;
 
 USE P6_Dotnet;
 
+-- Table des produits
 CREATE TABLE Produit (
   id INT PRIMARY KEY,
-  nom VARCHAR(255),
-  version_actuelle VARCHAR(255)
+  nom VARCHAR(255)
 );
 
+-- Table des systèmes d'exploitation
 CREATE TABLE SystemeExploitation (
   id INT PRIMARY KEY,
   nom VARCHAR(255)
 );
 
+-- Table des versions
 CREATE TABLE Version (
   id INT PRIMARY KEY,
+  numero_version VARCHAR(255)
+);
+
+-- Table des versions de produits
+CREATE TABLE ProduitVersion (
+  id INT PRIMARY KEY,
   produit_id INT,
-  numero_version VARCHAR(255),
-  FOREIGN KEY (produit_id) REFERENCES Produit(id)
-);
-
-CREATE TABLE VersionSystemeExploitation (
   version_id INT,
-  systeme_exploitation_id INT,
-  PRIMARY KEY (version_id, systeme_exploitation_id),
-  FOREIGN KEY (version_id) REFERENCES Version(id),
-  FOREIGN KEY (systeme_exploitation_id) REFERENCES SystemeExploitation(id)
+  FOREIGN KEY (produit_id) REFERENCES Produit(id),
+  FOREIGN KEY (version_id) REFERENCES Version(id)
 );
 
+-- Table pour l'association entre versions et systèmes d'exploitation
+CREATE TABLE ProduitVersionSystemeExploitation (
+    id INT PRIMARY KEY,
+    produit_version_id INT NOT NULL REFERENCES ProduitVersion(id),
+    systeme_exploitation_id INT NOT NULL REFERENCES SystemeExploitation(id)
+);
+
+
+-- Table des problèmes
 CREATE TABLE Probleme (
   id INT PRIMARY KEY,
   description VARCHAR(255),
   date_signalement DATE,
   statut VARCHAR(255) CHECK (statut IN ('En cours', 'Résolu')),
-  version_id INT,
-  FOREIGN KEY (version_id) REFERENCES Version(id)
+  produit_version_id INT,
+  FOREIGN KEY (produit_version_id) REFERENCES ProduitVersion(id)
 );
 
+-- Table des résolutions de problèmes
 CREATE TABLE Resolution (
   id INT PRIMARY KEY,
   probleme_id INT,
@@ -46,74 +57,133 @@ CREATE TABLE Resolution (
 );
 
 -- Insertion des données dans la table Produit
-INSERT INTO Produit (id, nom, version_actuelle) VALUES (1, 'Trader en Herbe', '1.3');
-INSERT INTO Produit (id, nom, version_actuelle) VALUES (2, 'Maître des Investissements', '2.1');
-INSERT INTO Produit (id, nom, version_actuelle) VALUES (3, 'Planificateur d’Entraînement', '2.0');
-INSERT INTO Produit (id, nom, version_actuelle) VALUES (4, 'Planificateur d’Anxiété Sociale', '1.1');
+INSERT INTO Produit (id, nom) VALUES 
+(1, 'Trader en Herbe'),
+(2, 'Maître des Investissements'),
+(3, 'Planificateur d’Entraînement'),
+(4, 'Planificateur d’Anxiété Sociale');
 
 -- Insertion des données dans la table SystemeExploitation
-INSERT INTO SystemeExploitation (id, nom) VALUES (1, 'Linux');
-INSERT INTO SystemeExploitation (id, nom) VALUES (2, 'MacOS');
-INSERT INTO SystemeExploitation (id, nom) VALUES (3, 'Windows');
-INSERT INTO SystemeExploitation (id, nom) VALUES (4, 'Android');
-INSERT INTO SystemeExploitation (id, nom) VALUES (5, 'iOS');
-INSERT INTO SystemeExploitation (id, nom) VALUES (6, 'Windows Mobile');
+INSERT INTO SystemeExploitation (id, nom) VALUES 
+(1, 'Linux'),
+(2, 'MacOS'),
+(3, 'Windows'),
+(4, 'Android'),
+(5, 'iOS'),
+(6, 'Windows Mobile');
 
 -- Insertion des données dans la table Version
-INSERT INTO Version (id, produit_id, numero_version) VALUES (1, 1, '1.0');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (2, 1, '1.1');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (3, 1, '1.2');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (4, 1, '1.3');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (5, 2, '1.0');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (6, 2, '2.0');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (7, 2, '2.1');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (8, 3, '1.0');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (9, 3, '1.1');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (10, 3, '2.0');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (11, 4, '1.0');
-INSERT INTO Version (id, produit_id, numero_version) VALUES (12, 4, '1.1');
+INSERT INTO Version (id, numero_version) VALUES 
+(1, '1.0'),
+(2, '1.1'),
+(3, '1.2'),
+(4, '1.3'),
+(5, '2.0'),
+(6, '2.1');
 
--- Insertion des données dans la table VersionSystemeExploitation
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (1, 1), (1, 2);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (2, 1), (2, 2), (2, 3);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (4, 1), (4, 2), (4, 3), (4, 4);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (5, 1), (5, 2);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (6, 1), (6, 2), (6, 3);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (7, 1), (7, 2), (7, 3), (7, 4);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (8, 1), (8, 2);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (10, 1), (10, 2), (10, 3), (10, 4);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (11, 1), (11, 2), (11, 3), (11, 4);
-INSERT INTO VersionSystemeExploitation (version_id, systeme_exploitation_id) VALUES (12, 1), (12, 2), (12, 3), (12, 4);
+-- Insertion des données dans la table ProduitVersion
+INSERT INTO ProduitVersion (id, produit_id, version_id) VALUES 
+(1, 1, 1), -- Trader en Herbe - 1.0
+(2, 1, 2), -- Trader en Herbe - 1.1
+(3, 1, 3), -- Trader en Herbe - 1.2
+(4, 1, 4), -- Trader en Herbe - 1.3
+(5, 2, 1), -- Maître des Investissements - 1.0
+(6, 2, 5), -- Maître des Investissements - 2.0
+(7, 2, 6), -- Maître des Investissements - 2.1
+(8, 3, 1), -- Planificateur d’Entraînement - 1.0
+(9, 3, 2), -- Planificateur d’Entraînement - 1.1
+(10, 3, 5), -- Planificateur d’Entraînement - 2.0
+(11, 4, 1), -- Planificateur d’Anxiété Sociale - 1.0
+(12, 4, 2); -- Planificateur d’Anxiété Sociale - 1.1
+
+
+
+-- Insertion des données dans la table ProduitVersionSystemeExploitation
+	-- Trader en Herbe
+INSERT INTO ProduitVersionSystemeExploitation (id, produit_version_id, systeme_exploitation_id) VALUES
+(1, 1, 1), -- 1.0 Linux
+(2, 1, 3), -- 1.0 Windows
+(3, 2, 1), -- 1.1 Linux
+(4, 2, 2), -- 1.1 MacOS
+(5, 2, 3), -- 1.1 Windows
+(6, 3, 1), -- 1.2 Linux
+(7, 3, 2), -- 1.2 MacOS
+(8, 3, 3), -- 1.2 Windows
+(9, 3, 4), -- 1.2 Android
+(10, 3, 5), -- 1.2 iOS
+(11, 3, 6), -- 1.2 Windows Mobile
+(12, 4, 2), -- 1.3 MacOS
+(13, 4, 3), -- 1.3 Windows
+(14, 4, 4), -- 1.3 Android
+(15, 4, 5); -- 1.3 iOS
+
+	-- Maître des Investissements
+INSERT INTO ProduitVersionSystemeExploitation (id, produit_version_id, systeme_exploitation_id) VALUES
+(16, 5, 2), -- 1.0 MacOS
+(17, 5, 5), -- 1.0 iOS
+(18, 6, 2), -- 2.0 MacOS
+(19, 6, 4), -- 2.0 Android
+(20, 6, 5), -- 2.0 iOS
+(21, 7, 2), -- 2.1 MacOS
+(22, 7, 3), -- 2.1 Windows
+(23, 7, 4), -- 2.1 Android
+(24, 7, 5); -- 2.1 iOS
+
+	-- Planificateur d’Entraînement
+INSERT INTO ProduitVersionSystemeExploitation (id, produit_version_id, systeme_exploitation_id) VALUES
+(25, 8, 1), -- 1.0 Linux
+(26, 8, 2), -- 1.0 MacOS
+(27, 9, 1), -- 1.1 Linux
+(28, 9, 2), -- 1.1 MacOS
+(29, 9, 3), -- 1.1 Windows
+(30, 9, 4), -- 1.1 Android
+(31, 9, 5), -- 1.1 iOS
+(32, 9, 6), -- 1.1 Windows Mobile
+(33, 10, 2), -- 2.0 MacOS
+(34, 10, 3), -- 2.0 Windows
+(35, 10, 4), -- 2.0 Android
+(36, 10, 5); -- 2.0 iOS
+
+	-- Planificateur d’Anxiété Sociale
+INSERT INTO ProduitVersionSystemeExploitation (id, produit_version_id, systeme_exploitation_id) VALUES
+(37, 11, 2), -- 1.0 MacOS
+(38, 11, 3), -- 1.0 Windows
+(39, 11, 4), -- 1.0 Android
+(40, 11, 5), -- 1.0 iOS
+(41, 12, 2), -- 1.1 MacOS
+(42, 12, 3), -- 1.1 Windows
+(43, 12, 4), -- 1.1 Android
+(44, 12, 5); -- 1.1 iOS
+
 
 -- Insertion des données dans la table Probleme
-INSERT INTO Probleme (id, description, date_signalement, statut, version_id) VALUES
-(1, 'L''application se bloque lors de l''ouverture du graphique des actions.', '2023-03-01', 'En cours', 1),
-(2, 'Les notifications de prix ne sont pas envoyées.', '2023-03-02', 'En cours', 6), 
-(3, 'L''application se ferme lors de l''ajout d''un nouvel entraînement.', '2023-03-03', 'Résolu', 8),
-(4, 'Les rappels ne sont pas affichés à l''heure prévue.', '2023-03-04', 'Résolu', 11),
-(5, 'L''application ne se met pas à jour avec les dernières données du marché.', '2023-03-05', 'Résolu', 2),
-(6, 'L''application se bloque lors de l''ouverture du tableau de bord.', '2023-03-06', 'En cours', 7), 
-(7, 'L''application ne sauvegarde pas les paramètres de l''utilisateur.', '2023-03-07', 'Résolu', 9),
-(8, 'L''application ne synchronise pas les données entre différents appareils.', '2023-03-08', 'Résolu', 12),
-(9, 'L''application ne charge pas les graphiques des actions.', '2023-03-09', 'Résolu', 3),
-(10, 'L''application ne permet pas de changer le mot de passe.', '2023-03-10', 'En cours', 5), 
-(11, 'Les calculs de charge d''entraînement sont erronés.', '2023-03-11', 'Résolu', 10), 
-(12, 'L''application ne permet pas de supprimer un rappel.', '2023-03-12', 'En cours', 11),
-(13, 'L''application ne permet pas de se déconnecter.', '2023-03-13', 'En cours', 4),
-(14, 'L''application ne permet pas de modifier le profil de l''utilisateur.', '2023-03-14', 'En cours', 6), 
-(15, 'L''application ne permet pas de partager un entraînement.', '2023-03-15', 'Résolu', 8),
-(16, 'L''application ne permet pas de programmer des rappels récurrents.', '2023-03-16', 'En cours', 12),
-(17, 'L''utilisateur ne reçoit aucune notification lors de variations significatives des marchés financiers.', '2023-03-17', 'Résolu', 1),
-(18, 'Les données du comparateur de fonds sont obsolètes.', '2023-03-18', 'Résolu', 7),
-(19, 'L''application ne permet pas de synchroniser les entraînements avec le calendrier de l''appareil.', '2023-03-19', 'Résolu', 9), 
-(20, 'L''application ne permet pas de programmer des rappels à des heures précises.', '2023-03-20', 'Résolu',11),
-(21, 'L''application ne permet pas de visualiser l''historique des transactions.', '2023-03-21', 'En cours',  4),
-(22, 'Les imports de relevé bancaire sont incomplets.', '2023-03-22', 'Résolu', 5),
-(23, 'L''application ne permet pas d''ajouter des exercices personnalisés.', '2023-03-23', 'En cours', 10),
-(24, 'L''application ne permet pas de supprimer un rappel.', '2023-03-24', 'Résolu', 12),
-(25, 'L''application ne permet pas de visualiser l''historique des transactions.', '2023-03-25', 'En cours', 1);
+INSERT INTO Probleme (id, description, date_signalement, statut, produit_version_id) VALUES
+(1, 'L''application se bloque lors de l''ouverture du graphique des actions.', '2023-03-01', 'En cours', 1), -- Trader en Herbe v1.0
+(2, 'Les notifications de prix ne sont pas envoyées.', '2023-03-02', 'En cours', 6), -- Maître des Investissements v2.0
+(3, 'L''application se ferme lors de l''ajout d''un nouvel entraînement.', '2023-03-03', 'Résolu', 8), -- Planificateur d’Entraînement v1.0
+(4, 'Les rappels ne sont pas affichés à l''heure prévue.', '2023-03-04', 'Résolu', 11), -- Planificateur d’Anxiété Sociale v1.0
+(5, 'L''application ne se met pas à jour avec les dernières données du marché.', '2023-03-05', 'Résolu', 2), -- Trader en Herbe v1.1
+(6, 'L''application se bloque lors de l''ouverture du tableau de bord.', '2023-03-06', 'En cours', 7), -- Maître des Investissements v2.1
+(7, 'L''application ne sauvegarde pas les paramètres de l''utilisateur.', '2023-03-07', 'Résolu', 9), -- Planificateur d’Entraînement v1.1
+(8, 'L''application ne synchronise pas les données entre différents appareils.', '2023-03-08', 'Résolu', 12), -- Planificateur d’Anxiété Sociale v1.1
+(9, 'L''application ne charge pas les graphiques des actions.', '2023-03-09', 'Résolu', 3), -- Trader en Herbe v1.2
+(10, 'L''application ne permet pas de changer le mot de passe.', '2023-03-10', 'En cours', 5), -- Maître des Investissements v1.0
+(11, 'Les calculs de charge d''entraînement sont erronés.', '2023-03-11', 'Résolu', 10), -- Planificateur d’Entraînement v2.0
+(12, 'L''application ne permet pas de supprimer un rappel.', '2023-03-12', 'En cours', 11), -- Planificateur d’Anxiété Sociale v1.0
+(13, 'L''application ne permet pas de se déconnecter.', '2023-03-13', 'En cours', 4), -- Trader en Herbe v1.3
+(14, 'L''application ne permet pas de modifier le profil de l''utilisateur.', '2023-03-14', 'En cours', 6), -- Maître des Investissements v2.0
+(15, 'L''application ne permet pas de partager un entraînement.', '2023-03-15', 'Résolu', 8), -- Planificateur d’Entraînement v1.0
+(16, 'L''application ne permet pas de programmer des rappels récurrents.', '2023-03-16', 'En cours', 12), -- Planificateur d’Anxiété Sociale v1.1
+(17, 'L''utilisateur ne reçoit aucune notification lors de variations significatives des marchés financiers.', '2023-03-17', 'Résolu', 1), -- Trader en Herbe v1.0
+(18, 'Les données du comparateur de fonds sont obsolètes.', '2023-03-18', 'Résolu', 7), -- Maître des Investissements v2.1
+(19, 'L''application ne permet pas de synchroniser les entraînements avec le calendrier de l''appareil.', '2023-03-19', 'Résolu', 9), -- Planificateur d’Entraînement v1.1
+(20, 'L''application ne permet pas de programmer des rappels à des heures précises.', '2023-03-20', 'Résolu', 11), -- Planificateur d’Anxiété Sociale v1.0
+(21, 'L''application ne permet pas de visualiser l''historique des transactions.', '2023-03-21', 'En cours', 4), -- Trader en Herbe v1.3
+(22, 'Les imports de relevé bancaire sont incomplets.', '2023-03-22', 'Résolu', 5), -- Maître des Investissements v1.0
+(23, 'L''application ne permet pas d''ajouter des exercices personnalisés.', '2023-03-23', 'En cours', 10), -- Planificateur d’Entraînement v2.0
+(24, 'L''application ne permet pas de supprimer un rappel.', '2023-03-24', 'Résolu', 12), -- Planificateur d’Anxiété Sociale v1.1
+(25, 'L''application ne permet pas de visualiser l''historique des transactions.', '2023-03-25', 'En cours', 1); -- Trader en Herbe v1.0
+
 
 -- Insertion des données dans la table Resolution
 INSERT INTO Resolution (id, probleme_id, description, date_resolution) VALUES
